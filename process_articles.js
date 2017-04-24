1ade5d1bcbafca91156614599a314cd692172ff8
Sean <ut.seanm@gmail.com>
2017-04-24 00:35:51 -0500

    Add timestamps

--- process_articles.js
+++ process_articles.js
@@ -1,6 +1,30 @@
-function sort
+function parseDates ( articles ) {
+  articles.map(function(article) {
+    articleDate = new Date(article['date'] * 1000);
+    article['timestamp'] = articleDate.toLocaleString();
+  });
+  return articles;
+}
+
+function sortArticles( articles ) {
+  articles.sort(function(i, j) {
+    return i['group-rank'] - j['group-rank'];
+  });
+  articles['cutoff'] = articles.length / 2;
+
+  return articles;
+}
 
 function generateList( result ) {
+  result = result.map(function(xs) {
+    return sortArticles(xs);
+  });
+
+  result = result.map(function(xs) {
+    return parseDates(xs);
+  });
+
+
 	$( "#articles" ).html( nunjucks.render('./template.html', {groups: result} ))
 };
 
